0db38b131997e3e9ceeaf85e85cf48f8e6edef3e
sotfcodeur <belkoweb9718@gmail.com>
2021-12-25 15:00:23 +0100

    softcodeur:Zipkin Server init

---
+++ src/test/java/com/fstg/bookerzipkinserver/BookerZipkinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fstg.bookerzipkinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerZipkinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
